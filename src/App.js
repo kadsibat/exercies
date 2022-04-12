81169aa1e50dc9d557348c6d34d00694b3766331
Batuhan <yesiltas_sibel@hotmail.com>
2022-04-12 12:32:50 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import './App.css'
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<About/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
