aa18defe6a1b268d7f689244936da9c56114fcb0
Batuhan <yesiltas_sibel@hotmail.com>
2022-04-12 14:52:32 +0300

    g

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+
+import IconButton from '@mui/material/IconButton';
+
+import Button from "@mui/material/Button";
+import { useNavigate } from 'react-router-dom';
+
 
 const Navbar = () => {
+    const navigate =useNavigate()
+   
   return (
-    <div>
-      navbar
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+    <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            padding: "0 2rem",
+          }}
+        >
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 0 }}
+            onClick={()=>navigate("/")}
+          >
+            CONTEXT
+          </IconButton>
+          <Box>
+          <Button onClick={() => navigate("/")} color="inherit">login</Button>
+          <Button onClick={() => navigate("/about")} color="inherit">register</Button>
+
+          </Box>
+        </Toolbar>
+    </AppBar>
+  </Box>
   )
 }
 
